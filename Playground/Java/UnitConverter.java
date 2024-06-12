fb9fbb9634fc399f4557f13470715817de564e7e
Koronashi <109332110+Koronashi@users.noreply.github.com>
2024-06-13 07:56:05 +0800

    update Playground/Java/UnitConverter.java

--- Playground/Java/UnitConverter.java
+++ Playground/Java/UnitConverter.java
@@ -1,12 +1,39 @@
 public class UnitConverter {
   public static void main(String[] args) {
-    System.out.println(Length.meterTomile(2));
+    LengthConverter conv = new LengthConverter("KilometerToMeter");
+    System.out.println(conv.LengthConversion(1));
   }
 }
 
-class Length {
-  public static double meterTomile(double m) {
-    double a = m * 0.0006214;
-    return a;
+//Length Conversion
+class LengthConverter {
+  private static String heca;
+  LengthConverter(String measurements) {
+    heca = measurements;
+  }
+  
+  public double LengthConversion(double value) {
+    double answ = 0;
+    switch (heca) {
+      case "KilometerToMeter":
+        answ = value * 1000;
+      break;
+      case "MeterToCentimeter":
+        answ = value * 100;
+      break;
+      case "CentimeterToMillimeter":
+        answ = value * 10;
+      break;
+      case "MillimeterToCentimeter":
+        answ = value / 10;
+      break;
+      case "CentimeterToMeter":
+        answ = value / 100;
+      break;
+      case "MeterKilometer":
+        answ = value / 1000;
+      break;
+    }
+    return answ;
   }
 }
